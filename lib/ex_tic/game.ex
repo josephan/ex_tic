2907e81a023f3cc4d196547f843bb22bc7543efa
Joseph An <an.joseph891@gmail.com>
2017-11-12 02:43:02 -0500

    Add sample module and test

---
+++ lib/ex_tic/game.ex
@@ -0,0 +1,5 @@
+defmodule ExTic.Game do
+  def two do
+    1 + 1
+  end
+end
---
+++ test/ex_tic/game_test.exs
@@ -0,0 +1,7 @@
+defmodule ExTic.GameTest do
+  use ExUnit.Case
+
+  test "two" do
+    assert ExTic.Game.two() == 2
+  end
+end
